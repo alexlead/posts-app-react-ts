5b21984d79de23317a63076db246a780133a7ff2
Alexander Lead <a.lead@yandex.ru>
2023-12-02 20:00:09 +0300

    fix(main): authors links fixed

--- src/components/AuthorCard.tsx
+++ src/components/AuthorCard.tsx
@@ -15,7 +15,7 @@ const AuthorCard = ( {author}:IAuthorCard ) => {
         <div className="author__details__contacts">
         <div className="author__details__contacts__phone">{author.phone}</div>
         <div className="author__details__contacts__email"><a href={"mailto:"+author.email}>{author.email}</a></div>
-        <div className="author__details__contacts__website"><a href={author.website}>{author.website}</a></div>
+        <div className="author__details__contacts__website"><a href={"http://"+author.website}>{author.website}</a></div>
         </div>
         </div>
     </div>
